48b66caf15a7fdf3fd6982bd2c448a5697be6076
bodashideung <rizkym775@gmail.com>
2022-08-26 17:02:37 +0700

    testing

--- src/Context/moviesContext.js
+++ src/Context/moviesContext.js
@@ -6,13 +6,26 @@ export const MoviesContext = createContext();
 
 export function MoviesProvider(props) {
   const [input, setInput] = useState("");
-  const {trendingMovies} = useTrendingMovies();
+  // const {trendingMovies} = useTrendingMovies();
   const {trendingTV} = useTrendingTV();
   const {topMovies} = useTopMovies();
   const [searchResult, setSearchResult] = useState([]);
   const [media, setMedia] = useState({ media: "movie" });
   const [detail, setDetail] = useState({});
   const { pathname } = useLocation();
+
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  async function getTrendingMovies() {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data)
+    setTrendingMovies(data.results);
+  }
+
+  useEffect(() => getTrendingMovies, [])
 
   console.log(trendingMovies)
 
