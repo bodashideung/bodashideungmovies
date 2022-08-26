7e6109f7756da6d88b2526fc4620ea2e163074fd
bodashideung <rizkym775@gmail.com>
2022-08-26 13:23:57 +0700

    adding hooks at component

--- src/components/sections/trending-section/trendingMovies/TrendingMovies.jsx
+++ src/components/sections/trending-section/trendingMovies/TrendingMovies.jsx
@@ -1,12 +1,24 @@
-import React, {useContext} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import Card from "../../../card/Card";
-import { MoviesContext } from "../../../../Context/moviesContext";
+// import { MoviesContext } from "../../../../Context/moviesContext";
 import {SplideSlide} from "@splidejs/react-splide";
 import '@splidejs/react-splide/css'
 
 function TrendingMovies() {
-const {trendingMovies} = useContext(MoviesContext);
-console.log(trendingMovies)
+// const {trendingMovies} = useContext(MoviesContext);
+// console.log(trendingMovies)
+
+const [trendingMovies, setTrendingMovies] = useState([]);
+
+async function getTrendingMovies() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`
+  );
+  const data = await response.json();
+  setTrendingMovies(data.results);
+}
+
+useEffect(() => getTrendingMovies, [])
 
   return (
     <React.Fragment>
