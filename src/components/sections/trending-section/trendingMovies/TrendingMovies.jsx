edbc326b8467124fb5797abf160a3d1ba75489d5
bodashideung <rizkym775@gmail.com>
2022-08-26 15:36:14 +0700

    fixing

--- src/components/sections/trending-section/trendingMovies/TrendingMovies.jsx
+++ src/components/sections/trending-section/trendingMovies/TrendingMovies.jsx
@@ -1,35 +1,11 @@
-import React, {useContext, useState, useEffect} from "react";
+import React, {useContext} from "react";
 import Card from "../../../card/Card";
-// import { MoviesContext } from "../../../../Context/moviesContext";
+import { MoviesContext } from "../../../../Context/moviesContext";
 import {SplideSlide} from "@splidejs/react-splide";
 import '@splidejs/react-splide/css'
 
 function TrendingMovies() {
-// const {trendingMovies} = useContext(MoviesContext);
-// console.log(trendingMovies)
-
-const [trendingMovies, setTrendingMovies] = useState([]);
-const [isLoading, setIsLoading] = useState(true);
-
-async function getTrendingMovies() {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`
-  );
-  const data = await response.json();
-  console.log(data)
-  setTrendingMovies(data.results);
-  setIsLoading(false);
-}
-
-useEffect(() => getTrendingMovies, [isLoading])
-
-if (isLoading) {
-  return (
-    <div>
-      loading...
-    </div>
-  )
-}
+const {trendingMovies} = useContext(MoviesContext);
 
   return (
     <React.Fragment>
