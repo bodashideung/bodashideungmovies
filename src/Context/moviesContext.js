18630f86af3dc069a664cee77762f72ae7db86f4
bodashideung <rizkym775@gmail.com>
2022-08-26 18:48:28 +0700

    fixing bug

--- src/Context/moviesContext.js
+++ src/Context/moviesContext.js
@@ -6,7 +6,7 @@ export const MoviesContext = createContext();
 
 export function MoviesProvider(props) {
   const [input, setInput] = useState("");
-  // const {trendingMovies} = useTrendingMovies();
+  const {trendingMovies} = useTrendingMovies();
   const {trendingTV} = useTrendingTV();
   const {topMovies} = useTopMovies();
   const [searchResult, setSearchResult] = useState([]);
@@ -14,24 +14,24 @@ export function MoviesProvider(props) {
   const [detail, setDetail] = useState({});
   const { pathname } = useLocation();
 
-  const [trendingMovies, setTrendingMovies] = useState([]);
+  // const [trendingMovies, setTrendingMovies] = useState([]);
 
   
-  useEffect(() => {
+  // useEffect(() => {
     
-    async function getTrendingMovies() {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`
-      );
-      const data = await response.json();
-      console.log(data)
-      setTrendingMovies(data.results);
-    }
+  //   async function getTrendingMovies() {
+  //     const response = await fetch(
+  //       `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`
+  //     );
+  //     const data = await response.json();
+  //     console.log(data)
+  //     setTrendingMovies(data.results);
+  //   }
 
-    getTrendingMovies()
-  }, [])
+  //   getTrendingMovies()
+  // }, [])
 
-  console.log(trendingMovies)
+  // console.log(trendingMovies)
 
   useEffect(() => toTop, [pathname]);
 
--- src/hooks/moviesHooks.js
+++ src/hooks/moviesHooks.js
@@ -4,16 +4,18 @@ import { useState, useEffect } from "react";
 export const useTrendingMovies = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
 
-  async function getTrendingMovies() {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`
-    );
-    const data = await response.json();
-    console.log(data)
-    setTrendingMovies(data.results);
-  }
-
-  useEffect(() => getTrendingMovies, [])
+  
+  useEffect(() => {
+    async function getTrendingMovies() {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data)
+      setTrendingMovies(data.results);
+    }
+    getTrendingMovies()
+  }, [])
 
   return {trendingMovies}
 };
@@ -22,17 +24,19 @@ export const useTrendingMovies = () => {
 export const useTrendingTV = () => {
     const [trendingTV, setTrendingTV] = useState([]);
 
-    async function getTrendingTV() {
-        const response = await fetch(
-            `https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_API_KEY}`
-        )
-
-        const data = await response.json();
-
-        setTrendingTV(data.results);
-    }
-
-    useEffect(() => getTrendingTV, [])
+    
+    useEffect(() => {
+      async function getTrendingTV() {
+          const response = await fetch(
+              `https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_API_KEY}`
+          )
+  
+          const data = await response.json();
+  
+          setTrendingTV(data.results);
+      }
+      getTrendingTV()
+    }, [])
 
     return {trendingTV}
 }
@@ -40,16 +44,18 @@ export const useTrendingTV = () => {
 export const useTopMovies = () => {
     const [topMovies, setTopMovies] = useState([]);
     
-    async function getTopMovies() {
-        const response = await fetch(
-            `
-            https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}`
-            );
-            const data = await response.json();
-            setTopMovies(data.results);
-        }
-        
-        useEffect(() => getTopMovies, []);
+    
+    useEffect(() => {
+          async function getTopMovies() {
+              const response = await fetch(
+                  `
+                  https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}`
+                  );
+                  const data = await response.json();
+                  setTopMovies(data.results);
+              }
+          getTopMovies()
+        }, []);
 
     return {topMovies}
 }
